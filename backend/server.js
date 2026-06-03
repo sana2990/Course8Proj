eeb7d3578f90f75a3f9aaeb4b6171b1ab0b02b49
sana2990 <sanaasad2990@gmail.com>
2026-06-03 08:37:09 +0530

    Changed some files path and added userSchema

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import mongoose from "mongoose";
+
+const app = new express();
+app.listen(5002, () => {
+    console.log("server: 5002");
+})
+
+mongoose.connect("mongodb://localhost:27017");
+
+const db = mongoose.connection;
+db.on("open", () => {
+    console.log("database connection is successful");
+})
+
+db.on("error",() =>{
+    console.log("database connection not successful");
+})
+
+app.get("/products", async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({
+            message: "Error fetching products",
+            error: error.message
+        });
+    }
+});
